041cad78002930d08830707f1dd1b61e84fffd7f
Guilherme Resende da Rocha <202402075365@alunos.ibmec.edu.br>
2024-10-10 10:51:02 -0300

    aula 10/10

--- src/components/NovoComponente/NovoComponente.jsx
+++ src/components/NovoComponente/NovoComponente.jsx
@@ -1,3 +1,10 @@
-const NovoComponente = () => "Texto do Componente";
+import { Container, Texto, Titulo } from "./Style";
+
+const NovoComponente = (props) => (
+    <Container>
+        <Titulo>{props.titulo}</Titulo>
+        <Texto>{props.texto}</Texto>
+    </Container>
+);
 
 export default NovoComponente;
--- src/pages/SobreNos.jsx
+++ src/pages/SobreNos.jsx
@@ -3,18 +3,40 @@ import Base from "./Base"
 import { auth } from "../config/Firebase";
 import { useEffect } from "react";
 import NovoComponente from "../components/NovoComponente/NovoComponente";
+import ListContainer from "../components/ListContainer/ListContainer";
+
+const meusDados = [
+    {
+        titulo: "Um título",
+        texto: "Um texto grande."
+    },
+    {
+        titulo: "Outro título",
+        texto: "Outro texto grande."
+    }
+]
 
 const SobreNos = () => {
 
     return (
         <>
-        <Base>
-          <h1>
-         Sobre Nós
-          </h1>
-          <br/>
-        </Base>
-        <NovoComponente/>
+            <Base>
+                <h1>
+                Sobre Nós
+                </h1>
+                <br/>
+            <ListContainer>
+                {
+                    meusDados.map((ele, i) => (
+                        <NovoComponente
+                            key={i}
+                            titulo={ele.titulo}
+                            texto={ele.texto}
+                        />
+                    ))
+                }
+            </ListContainer>
+            </Base>
         </>
       )
 };
