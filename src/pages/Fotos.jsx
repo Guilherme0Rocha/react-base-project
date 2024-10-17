ce478a2151286c9f40e65e8a2d9a5958e62245b3
Guilherme Resende da Rocha <202402075365@alunos.ibmec.edu.br>
2024-10-17 10:44:22 -0300

    aula

--- src/pages/Fotos.jsx
+++ src/pages/Fotos.jsx
@@ -1,26 +1,29 @@
+import { useParams } from "react-router-dom";
 import FotoCard from "../components/FotoCard/FotoCard";
 import ListContainer from "../components/ListContainer/ListContainer";
 import dados from "../data/computadores.json";
 import Protegida from "./Protegida";
 
 const Fotos = () => {
- 
+  const { id } = useParams();
+  const dadosFiltrados = dados.filter(
+    (elemento) => elemento.id === parseInt(id) || !id
+  )
+
   return (
-      <Protegida>
-        <ListContainer>
-          {dados.map(
-            (el, index) => (
-              <FotoCard 
-                key={index}
-                titulo={el.nome}
-                src={`${process.env.PUBLIC_URL}${el.url}`}
-                link_original={el.link_original}
-                agradecimento={el.agradecimento}
-              />
-              )
-          )}
-          </ListContainer>
-      </Protegida>      
+    <ListContainer>
+    {dadosFiltrados.map(
+      (el, index) => (
+        <FotoCard 
+          key={index}
+          titulo={el.nome}
+          src={`${process.env.PUBLIC_URL}${el.url}`}
+          link_original={el.link_original}
+          agradecimento={el.agradecimento}
+        />
+        )
+    )}
+    </ListContainer>   
  )
 };
 
